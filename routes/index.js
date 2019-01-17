6e4c0e8b0a59bb2195cc9661d71382da0c7241d4
zvenzven <zvenzuehlsdorf@googlemail.com>
2019-01-17 16:15:43 +0100

    Change router config

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+  res.render('index')
+})
 
-module.exports = router;
+router.get('/:template', function(req, res, next) {
+  res.render(req.params.template)
+})
+
+module.exports = router
